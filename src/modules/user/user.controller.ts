af204561001efaff1eccf6034c8d245f7333a356
ashikurriyal <riyal.ashikur@gmail.com>
2026-09-06 16:52:18 +0600

    feat: get user profile based on logged in user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,9 @@ import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import jwt from "jsonwebtoken"
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
@@ -21,6 +24,20 @@ const registerUser = catchAsync(async (req: Request, res: Response, next: NextFu
 
 const getMyProfile = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
+    const { accessToken } = req.cookies;
+    const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret)
+
+    if (typeof verifiedToken === "string") {
+        throw new Error(verifiedToken);
+    }
+    const profile = await userService.getMyProfileFromDB(verifiedToken.id)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User profile fetched successfully",
+        data: { profile }
+    })
 })
 
 
