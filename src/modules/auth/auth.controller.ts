6296a5cc0d53c3e4a597d961dc65362151b75241
ashikurriyal <riyal.ashikur@gmail.com>
2026-09-07 20:23:11 +0600

    feat:implement refresh token api to get new access token with refresh token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,29 +7,58 @@ import { sendResponse } from "../../utils/sendResponse";
 const loginUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
-    const {accessToken, refreshToken} = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
 
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: false,
         sameSite: "none",
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24 // 24 hour or 1 day
     })
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: false,
         sameSite: "none",
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 day
     })
 
     sendResponse(res, {
         success: true,
-        statusCode: httpStatus.CREATED,
-        message: "User login successfully",
-        data: {accessToken, refreshToken}
+        statusCode: httpStatus.OK,
+        message: "User logged in successfully",
+        data: { accessToken, refreshToken }
+    })
+})
+
+
+const refreshToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+    const { accessToken } = await authService.refreshToken(refreshToken);
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        // secure: process.env.NODE_ENV === "production",
+        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 1000 * 60 * 60 * 24 // 24 hour or 1 day
+    })
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Token Refreshed Successfully",
+        data: {
+            accessToken
+        }
     })
 })
 
 export const authController = {
-    loginUser
+    loginUser,
+    refreshToken
 }
