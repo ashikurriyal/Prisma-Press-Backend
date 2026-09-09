d285ddd65b1b456431d1765cc88434d010c0ae9d
ashikurriyal <riyal.ashikur@gmail.com>
2026-09-09 23:46:14 +0600

    refactor: cleanup the controller code

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -43,8 +43,6 @@ const refreshToken = catchAsync(async (req: Request, res: Response, next: NextFu
         httpOnly: true,
         secure: false,
         sameSite: "none",
-        // secure: process.env.NODE_ENV === "production",
-        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24 // 24 hour or 1 day
     })
 
