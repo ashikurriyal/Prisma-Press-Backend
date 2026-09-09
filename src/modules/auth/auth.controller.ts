2b5f9e68304b81ba01bcacc2c7be4bbb5f560741
ashikurriyal <riyal.ashikur@gmail.com>
2026-09-09 23:46:40 +0600

    refactor: cleanup the controller code

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -21,8 +21,6 @@ const loginUser = catchAsync(async (req: Request, res: Response, next: NextFunct
         httpOnly: true,
         secure: false,
         sameSite: "none",
-        // secure: process.env.NODE_ENV === "production",
-        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 day
     })
 
