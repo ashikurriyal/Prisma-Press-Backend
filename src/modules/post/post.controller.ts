283c67e3fc02ab26b951a57188bc59dec2e74bab
ashikurriyal <riyal.ashikur@gmail.com>
2026-09-15 00:24:34 +0600

    feat: implementing get post statistics api and using prisma aggregate function

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -81,7 +81,14 @@ const deletePost = catchAsync(async (req: Request, res: Response, next: NextFunc
 
 })
 const getPostsStats = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const result = await postService.getPostsStats()
 
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Post stats retrieved successfully",
+        data: result
+    })
 })
 const getMyPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const authorId = req.user?.id;
